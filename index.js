9c23783f33f0c6ee487e4eb6f2ed1796ef7893c4
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-06-07 13:32:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return (`This Saturday, I want to ${activity}!` )
+}
+saturdayFun("bathe my dog!");
+function mondayWork(activity= 'go to the office'){
+    return (`This Monday, I will ${activity}.`)
+}
+mondayWork("work from home")
+function wrapAdjective(style = "*") {
+    return function(adjective = "special") {
+        return `You are ${style}${adjective}${style}!`;    }
+}
